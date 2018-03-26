4fe772516691a58437f07c37683a85756b4bbbcc
zmbaker13 <zeth.baker@trimedx.com>
2018-03-26 17:30:13 -0400

    Commit

--- Equipment Domain Update Match Customer.js
+++ Equipment Domain Update Match Customer.js
@@ -1,10 +1,11 @@
-var grLegacy = new GlideRecord('u_cmdb_ci_equipment');
-grLegacy.addEncodedQuery('sys_domainNSAMEASu_account.sys_domain^u_account.active=true^u_account=743bda8bdbb6f6003c9a7aa9bf9619bb');
-grLegacy.setWorkflow(false);
-grLegacy.queryNoDomain();
-while (grLegacy.next()) {
-		grLegacy.sys_domain=grLegacy.u_account.sys_domain;
-		grLegacy.update();
+var grEquipDom = new GlideRecord('u_cmdb_ci_equipment');
+grEquipDom.addEncodedQuery('sys_domainNSAMEASu_account.sys_domain');
+grEquipDom.setWorkflow(false);
+grEquipDom.setAutoSysFields(false);
+grEquipDom.queryNoDomain();
+while (grEquipDom.next()) {
+		grEquipDom.sys_domain=grEquipDom.u_account.sys_domain;
+		grEquipDom.update();
 	}  
 
 
@@ -18,3 +19,29 @@ while (grLegacy.next()) {
 		grLegacy.assignment_group=grLegacy.department.u_default_assignment_group;
 		grLegacy.update();
 	}  
+
+
+var grWoDom = new GlideRecord('wm_order');
+grWoDom.addEncodedQuery('sys_domainNSAMEASu_account.sys_domain');
+grWoDom.setWorkflow(false);
+grWoDom.setAutoSysFields(false);
+grWoDom.queryNoDomain();
+while (grWoDom.next()) {
+		grWoDom.sys_domain=grWoDom.u_account.sys_domain;
+		grWoDom.update();
+	}  
+
+
+
+var grEquipDom = new GlideRecord('u_cmdb_ci_equipment');
+grEquipDom.addEncodedQuery("u_device_statusIN1,4^u_disposition_dateLIKE1900");
+grEquipDom.setLimit(5);
+grEquipDom.setWorkflow(false);
+grEquipDom.setAutoSysFields(false);
+grEquipDom.queryNoDomain();
+while (grEquipDom.next()) {
+		grEquipDom.u_disposition_date="";
+		grEquipDom.update();
+	}  
+
+	205,140
